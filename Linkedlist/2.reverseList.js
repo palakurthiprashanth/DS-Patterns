dab4293a89af45b03d443567a14a95f82206f388
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-10-25 09:50:04 +0530

    Update 2.reverseList.js

--- Linkedlist/2.reverseList.js
+++ Linkedlist/2.reverseList.js
@@ -21,3 +21,17 @@
     list.insertAtHead(1);
     list.insertAtHead(0);
     console.log(reverse(list));
+
+ // Reccursion solution
+ function reverselistRecc(list, prevNode=null, currentNode, nextNode) {
+        if (currentNode === null) {
+            list.head= prevNode;
+            return list;
+        }
+        nextNode= currentNode.nextElement;
+        currentNode.nextElement= prevNode;
+        prevNode= currentNode;
+        currentNode= nextNode;
+        return reverselistRecc(list, prevNode, currentNode, nextNode);
+    }
+    console.log(reverselistRecc(list, null, list.head, null));
