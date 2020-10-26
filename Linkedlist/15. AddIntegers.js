cd8a4aee740a299ff47b082e7c3b30b77a237d1f
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-10-26 11:49:54 +0530

    Create 15. AddIntegers.js

---
+++ Linkedlist/15. AddIntegers.js
@@ -0,0 +1,21 @@
+function addIntegers(list1, list2) {
+    let carry= 0;
+    let result = new LinkedList();
+
+    if (list1 || list2 || carry>0) {
+        let first= list1 ?list1.data: 0;
+        let second= list2 ? list2.data: 0;
+
+        let sum= first+second+carry;
+        result.insertAtTail(sum%10);
+        carry= Math.floor(sum/10);
+
+        if (list1) {
+            list1= list1.nextElement;
+        }
+        if(list2) {
+            list2= list2.nextElement;
+        }
+    }
+    return result;
+}
