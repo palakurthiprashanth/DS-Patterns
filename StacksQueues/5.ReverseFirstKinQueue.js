bc8d21a3e2c24bb5fd1de6cb541ee76336afadf2
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-10-28 08:07:00 +0530

    Update 5.ReverseFirstKinQueue.js

--- StacksQueues/5.ReverseFirstKinQueue.js
+++ StacksQueues/5.ReverseFirstKinQueue.js
@@ -16,3 +16,9 @@ function reverseK(queue, k) {
     }
     return queue;
 }
+/**
+Time Complexity #
+Overall, kk elements are dequeued, pushed to the stack, popped from it, and then enqueued. Additionally, n-kn−k elements are dequeued and enqueued to the queue. 
+On the assumption that each push, pop, enqueue or dequeue operation takes constant time, the time complexity of this function is O(n)O(n) as all nn elements have 
+to be processed with constant-time​ operations.
+**/
