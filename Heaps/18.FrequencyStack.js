14f3549b4fef2df489131ba33a560e2e244a04f6
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-08 18:58:47 +0530

    Create 18.FrequencyStack.js

---
+++ Heaps/18.FrequencyStack.js
@@ -0,0 +1,44 @@
+class FrequencyStack {
+    constructor() {
+        this.freqMap= {};
+        this.maxHeap= new Heap(function(a, b) {
+            return a[0]-b[0];
+        });
+    }
+    push(number) {
+        if (number in this.freqMap) {
+            this.freqMap[number]++;
+        }else {
+            this.freqMap[number]= 1;
+        }
+
+        this.maxHeap.push([this.freqMap[number], number]);
+    }
+    pop() {
+        let num= this.maxHeap.pop()[1];
+        this.freqMap[num]-=1;
+        if (this.freqMap[num] === 0) {
+            delete this.freqMap[num];
+        }
+        return num;
+    }
+}
+const frequencyStack = new FrequencyStack();
+frequencyStack.push(1);
+frequencyStack.push(2);
+frequencyStack.push(3);
+frequencyStack.push(2);
+frequencyStack.push(1);
+frequencyStack.push(2);
+frequencyStack.push(5);
+console.log(frequencyStack.pop());
+console.log(frequencyStack.pop());
+console.log(frequencyStack.pop());
+
+/**
+me complexity #
+The time complexity of push() and pop() is O(logN) where ‘N’ is the current number of elements in the heap.
+
+Space complexity #
+We will need O(N)O(N) space for the heap and the map, so the overall space complexity of the algorithm is O(N).
+**/
