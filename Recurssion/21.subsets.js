7d70e92fa9e73c40bb56df11c0bef635ca0084ea
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-15 16:33:34 +0530

    Update 21.subsets.js

--- Recurssion/21.subsets.js
+++ Recurssion/21.subsets.js
@@ -13,3 +13,20 @@ function printSubset(input, output) {
 }
 
 console.log(printSubset("abc", ''));
+
+
+function printSubsetArrays(input, output) {
+    if (input.length === 0) {
+        console.log(output);
+        return;
+    }
+
+    let op1= output;
+    let op2= output+input[0];
+    input= input.slice(1);
+
+    printSubsetArrays(input, op1);
+    printSubsetArrays(input, op2);
+}
+
+console.log(printSubsetArrays([1,2,3], []));
