bfa2bcfd30c917016cb97262b687993dc3d4d1f8
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-16 19:40:35 +0530

    Create 6.FindMissingNumber.js

---
+++ SearchingNSorting/6.FindMissingNumber.js
@@ -0,0 +1,15 @@
+function findMissingNumber(arr) {
+    let i= 0;
+    while(i<arr.length) {
+        let targetIndex= arr[i]-1;
+        if (arr[i] !== arr[targetIndex]) {
+            [arr[i], arr[targetIndex]]= [arr[targetIndex], arr[i]];// swapping to correct position.
+        }else {
+            i= i+1;
+        }
+    }
+    return arr;
+}
+console.log(findMissingNumber([3, 1, 5, 4, 2])); //[1,2,3,4,5];
+
+// TC: O(n).
