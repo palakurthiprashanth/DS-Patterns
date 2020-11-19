d6e00905ad5f143961c4df7f43946ee5e6d6dbe0
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-19 08:02:38 +0530

    Update 16.FindCeilingOfNumber.js

--- SearchingNSorting/16.FindCeilingOfNumber.js
+++ SearchingNSorting/16.FindCeilingOfNumber.js
@@ -27,9 +27,6 @@ function ceilingOfKey(arr, key) {
 
     while (start <=end) {
         let midIndex= Math.floor(start+(end-start)/2);
-        if (start === end) {
-            return start;
-        }
         if (key >arr[midIndex]) {
             start = midIndex+1;
         }else if (key <arr[midIndex]) {
@@ -38,6 +35,7 @@ function ceilingOfKey(arr, key) {
             return midIndex;
         }
     }
+    return start;
 }
 console.log(ceilingOfKey([4, 6, 10], 6));//1
 console.log(ceilingOfKey([1, 3, 8, 10, 15], 12));//4
@@ -45,3 +43,35 @@ console.log(ceilingOfKey([4, 6, 10], 17));//-1
 console.log(ceilingOfKey([4, 6, 10], -1));//0
 
 // TC: O(logn).
+
+
+
+// floor of a number.
+function search_floor_of_a_number(arr, key) {
+  if (key < arr[0]) { // if the 'key' is smaller than the smallest element
+    return -1;
+  }
+
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    mid = Math.floor(start + (end - start) / 2);
+    if (key < arr[mid]) {
+      end = mid - 1;
+    } else if (key > arr[mid]) {
+      start = mid + 1;
+    } else { // found the key
+      return mid;
+    }
+  }
+
+  // since the loop is running until 'start <= end', so at the end of the while loop, 'start === end+1'
+  // we are not able to find the element in the given array, so the next smaller number will be arr[end]
+  return end;
+}
+
+
+console.log(search_floor_of_a_number([4, 6, 10], 6));
+console.log(search_floor_of_a_number([1, 3, 8, 10, 15], 12));
+console.log(search_floor_of_a_number([4, 6, 10], 17));
+console.log(search_floor_of_a_number([4, 6, 10], -1));
