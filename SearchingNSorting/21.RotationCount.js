41add91a1669ae0b93a95086047126c6da5b2995
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-19 21:29:18 +0530

    Create 21.RotationCount.js

---
+++ SearchingNSorting/21.RotationCount.js
@@ -0,0 +1,16 @@
+/**
+Given an array of numbers which is sorted in ascending order and is rotated ‘k’ times around a pivot, find ‘k’.
+
+You can assume that the array does not have any duplicates.
+
+Example 1:
+
+Input: [10, 15, 1, 3, 8]
+Output: 2
+Explanation: The array has been rotated 2 times.
+**/
+
+
+
+It is same as Find pivot element in a sorted array , where we finfd the smallest value in rotated array , index of smallest value is which count of rotations,
+In above example , index of smallest value is 2, hence count is 2.
