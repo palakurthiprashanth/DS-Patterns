3a5851ff1c38c5e9a0e6003a7e200ef3a2cb30b5
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-11-23 01:01:26 +0530

    Create 1.ImplementGraph.js

---
+++ Graphs/1.ImplementGraph.js
@@ -0,0 +1,39 @@
+class Graph {
+    constructor(vertices) {
+        this.vertices= vertices;
+        this.list= [];
+
+        for (var i=0;i<vertices;i++) {
+            let temp= new LinkedList();
+            // Each vertex is a linkedlist.
+            this.list.push(temp);
+        }
+    }
+    addEdge(source, destination) {
+        if (source < this.vertices && destination < this.vertices) {
+            this.list[source].insertAtHead(destination);
+            // If undirected Graph
+            // this.list[destination].insertAtHead(source);
+        }
+    }
+    printGraph() {
+        let result= "";
+        for (var i=0;i<this.list.length;i++) {
+            result+="||"+i+"||";
+            let head= this.list[i].getHead();
+            while(head!==null) {
+                result+="=>"+head.data;
+                head= head.nextElement;
+            }
+            result+="=>"+null;
+            console.log(result);
+            result= "";
+        }
+    }
+}
+let g = new Graph(4);
+g.addEdge(0, 1);
+g.addEdge(0, 2);
+g.addEdge(1, 3);
+g.addEdge(2, 3);
+g.printGraph();
