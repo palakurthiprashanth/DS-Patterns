c9eb18f2e6894f3873446b38d233b7cdf682aa20
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-12-18 09:22:11 +0530

    Create 11.BSTToBalancedBST.js

---
+++ Trees/11.BSTToBalancedBST.js
@@ -0,0 +1,2 @@
+1. store values of inOrderPrint in array // it will be in. sorted manner. EX: https://github.com/palakurthiprashanth/DS-Patterns/blob/main/Trees/3.FindKthMaxinBST.js
+2. for this sorted array use https://github.com/palakurthiprashanth/DS-Patterns/blob/main/Trees/9.ConstructBalancedBSTFromArray.js
