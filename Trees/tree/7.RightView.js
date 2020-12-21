b981c7673b77ac3e8121b34685521639cd2d9628
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-12-21 09:37:55 +0530

    Create 7.RightView.js

---
+++ Trees/tree/7.RightView.js
@@ -0,0 +1,42 @@
+function tree_right_view(root) {
+  result = [];
+  if (root === null) {
+    return result;
+  }
+
+  const queue = new Deque();
+  queue.push(root);
+  while (queue.length > 0) {
+    const levelSize = queue.length;
+    for (i = 0; i < levelSize; i++) {
+      currentNode = queue.shift();
+      // if it is the last node of this level, add it to the result
+      if (i === levelSize - 1) {
+        result.push(currentNode);
+      }
+      // insert the children of current node in the queue
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+    }
+  }
+
+  return result;
+}
+
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+root.left.left.left = new TreeNode(3);
+result = tree_right_view(root);
+process.stdout.write('Tree right view: ');
+for (i = 0; i < result.length; i++) {
+  process.stdout.write(`${result[i].val} `);
+} // 12 1 5 3 
