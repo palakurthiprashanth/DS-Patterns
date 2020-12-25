31bfbc49e2219545f936b60984f24705d0cf112e
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-12-25 09:29:56 +0530

    Create 9.PermutationOfString.js

---
+++ Arrays/SlidingWindow/9.PermutationOfString.js
@@ -0,0 +1,68 @@
+/**
+Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+Permutation is defined as the re-arranging of the characters of the string. For example, “abc” has the following six permutations:
+
+abc
+acb
+bac
+bca
+cab
+cba
+If a string has ‘n’ distinct characters, it will have n! permutations.
+
+Example 1:
+
+Input: String="oidbcaf", Pattern="abc"
+Output: true
+Explanation: The string contains "bca" which is a permutation of the given pattern.
+
+**/
+
+function find_permutation(str, pattern) {
+  let windowStart = 0,
+    matched = 0,
+    charFrequency = {};
+
+  for (i = 0; i < pattern.length; i++) {
+    const chr = pattern[i];
+    if (!(chr in charFrequency)) {
+      charFrequency[chr] = 0;
+    }
+    charFrequency[chr] += 1;
+  }
+
+  // Our goal is to match all the characters from the 'charFrequency' with the current window
+  // try to extend the range [windowStart, windowEnd]
+  for (windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (rightChar in charFrequency) {
+      // Decrement the frequency of matched character
+      charFrequency[rightChar] -= 1;
+      if (charFrequency[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFrequency).length) {
+      return true;
+    }
+
+    // Shrink the sliding window
+    if (windowEnd >= pattern.length - 1) {
+      leftChar = str[windowStart];
+      windowStart += 1;
+      if (leftChar in charFrequency) {
+        if (charFrequency[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFrequency[leftChar] += 1;
+      }
+    }
+  }
+  return false;
+}
+
+
+console.log(`Permutation exist: ${find_permutation('oidbcaf', 'abc')}`); // true.
+console.log(`Permutation exist: ${find_permutation('odicf', 'dc')}`); // false.
