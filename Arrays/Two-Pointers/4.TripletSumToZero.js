c647195732e5aafcf8355eec625d33fb79cd6318
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-12-25 19:02:34 +0530

    Create 4.TripletSumToZero.js

---
+++ Arrays/Two-Pointers/4.TripletSumToZero.js
@@ -0,0 +1,42 @@
+/**
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+
+Example 1:
+
+Input: [-3, 0, 1, 2, -1, 1, -2]
+Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
+Explanation: There are four unique triplets whose sum is equal to zero.
+
+**/
+
+function search_triplets(arr) {
+  arr.sort((a, b) => a - b);
+  const triplets = [];
+  for (i = 0; i < arr.length; i++) {
+    
+    search_pair(arr, -arr[i], i + 1, triplets);
+  }
+
+  return triplets;
+}
+
+
+function search_pair(arr, target_sum, left, triplets) {
+  let right = arr.length - 1;
+  while (left < right) {
+    const current_sum = arr[left] + arr[right];
+    if (current_sum === target_sum) { // found the triplet
+      triplets.push([-target_sum, arr[left], arr[right]]);
+      left += 1;
+      right -= 1;
+     
+    } else if (target_sum > current_sum) {
+      left += 1; // we need a pair with a bigger sum
+    } else {
+      right -= 1; // we need a pair with a smaller sum
+    }
+  }
+}
+
+
+console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2]));
