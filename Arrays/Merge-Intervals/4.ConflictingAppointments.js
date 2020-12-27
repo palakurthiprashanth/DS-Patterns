5b9d045c27b7c4c3a0be0d4c533ab9236deeb58c
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-12-27 11:04:20 +0530

    Update 4.ConflictingAppointments.js

--- Arrays/Merge-Intervals/4.ConflictingAppointments.js
+++ Arrays/Merge-Intervals/4.ConflictingAppointments.js
@@ -1,3 +1,19 @@
+/**
+Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+
+Example 1:
+
+Appointments: [[1,4], [2,5], [7,9]]
+Output: false
+Explanation: Since [1,4] and [2,5] overlap, a person cannot attend both of these appointments.
+Example 2:
+
+Appointments: [[6,7], [2,4], [8,12]]
+Output: true
+Explanation: None of the appointments overlap, therefore a person can attend all of them.
+
+**/
+
 function can_attend_all_appointments(intervals) {
   intervals.sort((a, b) => a.start - b.start);
   for (i = 1; i < intervals.length; i++) {
